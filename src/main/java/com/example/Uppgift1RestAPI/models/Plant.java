f9f4b031e4dd3bbf797b71cbcd4a76aab61f29a0
karolina-svensson <karolinasvensson92@gmail.com>
2025-01-18 14:10:18 +0100

    La till konstruktor och getters och setters

--- src/main/java/com/example/Uppgift1RestAPI/models/Plant.java
+++ src/main/java/com/example/Uppgift1RestAPI/models/Plant.java
@@ -22,4 +22,79 @@ public class Plant {
     private enum Status {
         AVAILABLE, RESERVED, EXCHANGED, SOLD
     }
+
+    public Plant() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTrivialName() {
+        return trivialName;
+    }
+
+    public void setTrivialName(String trivialName) {
+        this.trivialName = trivialName;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getLightRequirement() {
+        return lightRequirement;
+    }
+
+    public void setLightRequirement(String lightRequirement) {
+        this.lightRequirement = lightRequirement;
+    }
+
+    public String getWaterRequirement() {
+        return waterRequirement;
+    }
+
+    public void setWaterRequirement(String waterRequirement) {
+        this.waterRequirement = waterRequirement;
+    }
+
+    public int[] getMaintainingDifficulty() {
+        return maintainingDifficulty;
+    }
+
+    public void setMaintainingDifficulty(int[] maintainingDifficulty) {
+        this.maintainingDifficulty = maintainingDifficulty;
+    }
+
+    public String getRequests() {
+        return requests;
+    }
+
+    public void setRequests(String requests) {
+        this.requests = requests;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
