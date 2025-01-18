2f4863e9304d66cc649bd389e136866ffd057d7b
karolina svensson <karolinasvensson92@gmail.com>
2025-01-18 13:37:03 +0100

    Initial commit

---
+++ src/main/java/com/example/Uppgift1RestAPI/Uppgift1RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Uppgift1RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uppgift1RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uppgift1RestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Uppgift1RestAPI/Uppgift1RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uppgift1RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uppgift1RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
