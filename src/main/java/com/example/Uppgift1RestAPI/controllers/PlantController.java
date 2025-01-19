5b120adbf47769730af10bd7dbb2669a499b85b7
karolina-svensson <karolinasvensson92@gmail.com>
2025-01-19 11:50:46 +0100

    La till PlantController

---
+++ src/main/java/com/example/Uppgift1RestAPI/controllers/PlantController.java
@@ -0,0 +1,43 @@
+package com.example.Uppgift1RestAPI.controllers;
+
+
+import com.example.Uppgift1RestAPI.models.Plant;
+import com.example.Uppgift1RestAPI.repositories.PlantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/plants")
+public class PlantController {
+    @Autowired
+    private PlantRepository plantRepository;
+
+    @PostMapping
+    public ResponseEntity<Plant> createPlant(@RequestBody Plant plant) {
+        Plant savedPlant = plantRepository.save(plant);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedPlant);
+    }
+    @GetMapping
+    public ResponseEntity<List<Plant>> getAllPlants() {
+        List<Plant> plants = plantRepository.findAll();
+        return ResponseEntity.ok(plants);
+    }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Plant> updatePlant(@PathVariable String id, @RequestBody Plant plant) {
+        Plant existingPlant = plantRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant not found"));
+     return ResponseEntity.ok(plantRepository.save(existingPlant));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Plant> deletePlant(@PathVariable String id) {
+        if(!plantRepository.existsById(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Plant not found");
+        }
+        plantRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
