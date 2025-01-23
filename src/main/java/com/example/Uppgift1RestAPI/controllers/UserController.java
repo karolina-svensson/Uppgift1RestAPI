239fe269d791ec4acdc96976b8fc413de5e0af1b
karolina-svensson <karolinasvensson92@gmail.com>
2025-01-23 15:39:00 +0100

    La till UserController och skapade PostMapping

---
+++ src/main/java/com/example/Uppgift1RestAPI/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.Uppgift1RestAPI.controllers;
+
+
+import com.example.Uppgift1RestAPI.models.User;
+import com.example.Uppgift1RestAPI.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+}
+}
+
