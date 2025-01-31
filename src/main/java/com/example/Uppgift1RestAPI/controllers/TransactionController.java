a2ca4b1f5c42e2a66f97cb10c92683d70adc62be
karolina-svensson <karolinasvensson92@gmail.com>
2025-01-31 13:37:09 +0100

    La till GetMapping och GetMapping (id) i TransactionController

--- src/main/java/com/example/Uppgift1RestAPI/controllers/TransactionController.java
+++ src/main/java/com/example/Uppgift1RestAPI/controllers/TransactionController.java
@@ -6,10 +6,10 @@ import com.example.Uppgift1RestAPI.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
@@ -21,5 +21,15 @@ public class TransactionController {
     public ResponseEntity<Transaction> createTransaction(@RequestBody Transaction transaction) {
         Transaction savedTransaction = transactionRepository.save(transaction);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTransaction);
+    }
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = transactionRepository.findAll();
+        return ResponseEntity.ok(transactions);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable String id) {
+        Transaction transaction = transactionRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Transaction not found"));
+        return ResponseEntity.ok(transaction);
     }
 }
